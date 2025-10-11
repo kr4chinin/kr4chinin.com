814b98b7ee7a15dd60b6efca59062aae420578e3
Ilya Kruchinin <kr4chinin@mail.ru>
2025-10-11 19:19:41 +0800

    feat: socials + avatar

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export enum Socials {
+	X = 'https://x.com/kr4chinin',
+	GITHUB = 'https://github.com/kr4chinin',
+	LINKED_IN = 'https://linkedin.com/in/kruchinin-ilya',
+	EMAIL = 'mailto:kr4chinin@icloud.com',
+}
