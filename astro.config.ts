d6d5e2335527032b49a2a7dbc332429c8a01d29d
Ilya Kruchinin <kr4chinin@mail.ru>
2025-10-11 17:57:33 +0800

    chore: tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
-// @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	server: {
 		port: 3000,
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
